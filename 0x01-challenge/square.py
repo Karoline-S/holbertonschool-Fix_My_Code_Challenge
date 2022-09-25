c33ff8a9a84ae532258e5d10d364fb0c03486e0a
Karoline-S <karoline.amy@gmail.com>
2022-09-25 23:24:59 +0000

    fix - method and class names

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-class square():
+class Square():
     
     width = 0
     height = 0
@@ -14,7 +14,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
